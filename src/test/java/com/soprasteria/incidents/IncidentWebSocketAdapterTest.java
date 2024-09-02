c099377915d939aefb8f1a515b8809faedf4519d
Johannes Brodwall <johannes@brodwall.com>
2024-09-03 01:27:25 +0200

    Refactor IncidentWebSocketAdapterTest

--- src/test/java/com/soprasteria/incidents/IncidentWebSocketAdapterTest.java
+++ src/test/java/com/soprasteria/incidents/IncidentWebSocketAdapterTest.java
@@ -5,6 +5,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.websocket.api.WebSocketAdapter;
 import org.eclipse.jetty.websocket.client.WebSocketClient;
 import org.eclipse.jetty.websocket.server.config.JettyWebSocketServletContainerInitializer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -17,26 +18,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class IncidentWebSocketAdapterTest {
 
+    private IncidentProcessor processor;
+    private WebSocketClient webSocketClient = new WebSocketClient();
+
+    public IncidentWebSocketAdapterTest() {
+        processor = new IncidentProcessor();
+    }
+
+    @BeforeEach
+    void setUp() throws Exception {
+        webSocketClient.start();
+    }
+
     @Test
     void shouldTransmitSnapshotsToClient() throws Exception {
-        var processor = new IncidentProcessor();
         var createIncident = new CreateIncidentEventDto()
                 .setIncidentId(UUID.randomUUID())
                 .setDescription("Test incident");
         processor.process(createIncident);
-
-        var server = new Server(0);
-        var context = new ServletContextHandler();
-        context.addServletContainerInitializer(new JettyWebSocketServletContainerInitializer(
-                (_, container) -> container.addMapping("/ws", (req, resp) -> new IncidentWebSocketAdapter(processor))
-        ));
-        server.setHandler(context);
-        server.start();
+        var server = createServer();
 
         var messages = new ArrayBlockingQueue<String>(10);
-
-        var webSocketClient = new WebSocketClient();
-        webSocketClient.start();
         webSocketClient.connect(new WebSocketAdapter() {
             @Override
             public void onWebSocketText(String message) {
@@ -51,6 +53,17 @@ public class IncidentWebSocketAdapterTest {
                 .setIncidentId(createIncident.getIncidentId())
                 .setDescription(createIncident.getDescription())
         ));
+    }
+
+    private Server createServer() throws Exception {
+        var server = new Server(0);
+        var context = new ServletContextHandler();
+        context.addServletContainerInitializer(new JettyWebSocketServletContainerInitializer(
+                (_, container) -> container.addMapping("/ws", (req, resp) -> new IncidentWebSocketAdapter(processor))
+        ));
+        server.setHandler(context);
+        server.start();
+        return server;
     }
 
 }
