5f82dcfc57018c16fee86c094860286015c6290c
Johannes Brodwall <johannes@brodwall.com>
2024-09-03 00:26:52 +0200

    🟢 IncidentEventProcessorTest.shouldCreateIncident

--- src/main/java/com/soprasteria/incidents/IncidentProcessor.java
+++ src/main/java/com/soprasteria/incidents/IncidentProcessor.java
@@ -1,13 +1,20 @@
 package com.soprasteria.incidents;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class IncidentProcessor {
-    public void process(CreateIncidentEventDto createEvent) {
+    private final List<IncidentSnapshotDto> snapshots = new ArrayList<>();
 
+    public void process(CreateIncidentEventDto createEvent) {
+        snapshots.add(new IncidentSnapshotDto()
+                .setIncidentId(createEvent.getIncidentId())
+                .setIncidentType(createEvent.getIncidentType())
+                .setDescription(createEvent.getDescription())
+        );
     }
 
     public List<IncidentSnapshotDto> list() {
-        return null;
+        return snapshots;
     }
 }
